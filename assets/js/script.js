cb701e00ac0970da92e561c9f0efc68e129308f5
Andrew Ralston <andrewralston35@gmail.com>
2020-05-14 13:33:17 +0000

    added quiz content and hid on initialising page using script.js

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+//start with score of 0
+let score = 0;
+//number of questions in each round
+let roundTotal = 5;
+//number of questions total
+let total = 10;
+//points given for each answer
+let point = 1;
+//maximum achievable score
+let maxScore = point * total;
+
+
+$(document).ready(function () {
+    //hide quiz on loading
+    $(".quiz-round").hide();
+    //show lading page content
+    $(".landing-page-content").show();
+});
+
+//hide landing page content and show round one when start button is clicked
+$(".btn-quiz").click(function () {
+    $(".landing-page-content").hide();
+    $(".round-one").show();
+});
